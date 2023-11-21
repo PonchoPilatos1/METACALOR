ffb8335fa17a28b2609b1938de1aca5beae7b07b
Ernesto0701 <125234968+Ernesto0701@users.noreply.github.com>
2023-11-20 22:50:41 -0600

    Add files via upload

---
+++ Controladores.php/controlador_login.php
@@ -0,0 +1,11 @@
+<?php
+
+if (!empty($_POST["buttonIngresar"])) {
+    if (empty($_POST["usuario"]) and empty($_POST["password"])) {
+        echo "Los campos esta vacios";
+    } else {
+
+    }
+}
+
+?>
